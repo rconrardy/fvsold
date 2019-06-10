dbbdf3d61a024ef7cc432744563d93d26c29ab05
rconrardy <conrardy@umich.edu>
2019-06-10 11:47:31 -0400

    just some last minutes saves before i reset my computer

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 visions = {
     "mainfoveal": {
         "ratio": 1/3,
-        "size": 100,
-        "jobs": ["log"]
+        "size": 124,
+        "jobs": ["log", "detection"]
     },
     "parafoveal": {
         "ratio": 2/3,
-        "size": 100,
+        "size": 124,
         "jobs": ["curr", "detection"]
     },
     "peripheral": {
         "ratio": 3/3,
-        "size": 100,
+        "size": 124,
         "jobs": ["gray"]
     }
 }
